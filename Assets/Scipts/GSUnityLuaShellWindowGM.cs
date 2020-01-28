9aac82663936ef566d9bee35c867d00e78167a0e
kavenGw <371905989@qq.com>
2020-01-28 19:30:13 +0800

    $r to reload and $pl to show package.loaded

--- Assets/Scipts/GSUnityLuaShellWindowGM.cs
+++ Assets/Scipts/GSUnityLuaShellWindowGM.cs
@@ -1,7 +1,10 @@
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Reflection;
+using Boo.Lang;
 using UnityEngine;
+using XLua;
 
 namespace GSUnityLuaShell
 {
@@ -58,24 +61,81 @@ namespace GSUnityLuaShell
             return methodInfo.Invoke(this,null) as object[];
         }
         
+        [GSUnityLuaShellGMItem(("pl"))]
+        public object[] Loaded()
+        {
+            return getValue("package.loaded");
+        }
+        
         [GSUnityLuaShellGMItem(("r"))]
         public object[] Reload()
         {
-            return null;
+            string command = @"
+            local all_loaded_packages = {}
+            local except_packages = {
+                ['package']=true,['coroutine']=true,
+                ['_G']=true,['string']=true,
+                ['table']=true,['math']=true,
+                ['debug']=true,['utf8']=true,
+                ['os']=true,['io']=true}
+            for packageName,_ in pairs(package.loaded) do
+                if not except_packages[packageName] then
+                    table.insert( all_loaded_packages, packageName )
+                end
+            end
+            for index,loaded_package in ipairs(all_loaded_packages) do
+                print(loaded_package)
+                package.loaded[loaded_package] = nil
+                require(loaded_package)
+            end
+            return all_loaded_packages
+            ";
+            return getValue(command);
         }
         
         [GSUnityLuaShellGMItem(("g"))]
         public object[] G()
         {
-            string command = @"
-                temp_value = _G
-                local result = {}  
-                for a,b in pairs(_G) do
-                    table.insert(result,string.format('%s: %s',a,b))
-                end
-                return table.concat(result,'\n')
-            ";
-            return Exec(command);
+            return getValue("_G");
+        }
+
+        private object[] getValue(string value)
+        {
+            object[] objects = Exec(value);
+            if (objects.Length <= 0)
+            {
+                return new object[]{$"value:{value} not exit"};
+            }
+
+            System.Collections.Generic.List<object> results = new System.Collections.Generic.List<object>();
+            printLuaValue(results,objects[0]);
+            return results.ToArray();
+        }
+
+        private void printLuaValue(System.Collections.Generic.List<object> result,object obj)
+        {
+            if (obj is LuaTable)
+            {
+                LuaTable table = obj as LuaTable;
+                foreach (var key in table.GetKeys())
+                {
+                    result.Add($"{key}    {table[key].ToString()}");
+                }
+
+                for (int i = 1; i <= int.MaxValue; i++)
+                {
+                    object value = table[i];
+                    if (value == null)
+                    {
+                        break;
+                    }
+                    result.Add($"{table[i].ToString()}");
+                }
+            }
+            else
+            {
+                result.Add(obj.ToString());
+            }
         }
     }
 
